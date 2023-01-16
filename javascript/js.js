408007c1374a08f88b79299f26e50491901c8cc8
jigar <jigar.k@simformsolutions.com>
2023-01-16 11:19:56 +0530

    Repository

---
+++ javascript/js.js
@@ -0,0 +1,10 @@
+function scroll_left(){
+    var left = document.querySelector('.subitems')
+    left.scrollBy(150,0)
+}
+
+
+function scroll_right(){
+    var right = document.querySelector('.subitems')
+    right.scrollBy(-150,0)
+}
